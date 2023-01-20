bf5786c7d62cb20c014cbb9e06937ff911916d8f
LucileSD <4316@holbertonschool.com>
2023-01-20 13:19:39 +0100

    Simple callback

--- 0x0C-Javascript_advanced/14-wikipedia.js
+++ 0x0C-Javascript_advanced/14-wikipedia.js
@@ -6,15 +6,15 @@ function createElement (data) {
 
 function queryWikipedia (callback) {
   let xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow&origin=*');
-  xhr.onload = function () {
-    if (xhr.status === 200) {
-      const response = JSON.parse(xhr.response);
-      const extract = response.query.pages[Object.keys(response.query.pages)[0]].extract;
-      callback(extract);
-    }
-  };
+  xhr.open("GET", 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow&origin=*');
   xhr.send();
+  xhr.onload = function () {
+    const response = JSON.parse(xhr.response);
+    const extract = response.query.pages[Object.keys(response.query.pages)[0]].extract;
+    callback(extract);
+  };
+  xhr.onerror = function () {
+    alert('Request failed');
+  }
 }
-
 queryWikipedia(createElement);
